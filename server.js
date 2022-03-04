667e44280a7dce4a74e9105060b71b3198b2e4c1
kirill <eltysevkirill70@gmail.com>
2022-03-05 01:29:21 +0900

    nice

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+const {Shopify} =require('@shopify/shopify-api') ;
 const express = require('express');
 const cors = require('cors')
 const { ethers } = require("ethers");
+// const { DataType }=require('@shopify/shopify-api')
 const bodyParser = require('body-parser');
 
 const contract = require('./MegaPrimatesToken.json')
+
 
 const contractAddress = "0xc61BF058b7D69f2a7b898DA87aC12562f02027e0";
 const abi = contract.abi;
@@ -26,6 +29,9 @@ const port = process.env.PORT || 5000;
 //     console.log(count)
 // }
 
+// const client = new Shopify.Clients.Rest('testtesttest111a.myshopify.com', "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");
+
+
 app.post("/login",async (req,res)=>{
     const {signature,address,count} = req.body
     const hash = await ethers.utils.keccak256(address+count)
@@ -37,6 +43,12 @@ app.post("/login",async (req,res)=>{
     }else{
         res.json(false)
     }
+    // const data = await client.post({
+    //   path: 'customers',
+    //   data: {"customer":{"first_name":"Steve","last_name":"Lastnameson","email":"steve.lastnameson@example.com","phone":"+15142546011","verified_email":true,"addresses":[{"address1":"123 Oak St","city":"Ottawa","province":"ON","phone":"555-1212","zip":"123 ABC","last_name":"Lastnameson","first_name":"Mother","country":"CA"}]}},
+    //   type: DataType.JSON,
+    // });
+    // console.log(data , "data______________________")
   })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
